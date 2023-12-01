9995574af181f62a34077a8e43b0efe54bd331e7
mpara <mpara@MISLAV>
2023-12-01 20:12:36 +0100

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace ConsoleApp2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine();
+        }
+    }
+}

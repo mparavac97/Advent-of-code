7beb86257e9ae64230edea6add9c3db3144550a8
mparavac <mparavac@gmail.com>
2023-12-02 18:10:26 +0100

    added game task

---
+++ AdventOfCode/Day2/GameTaskRunner.cs
@@ -0,0 +1,96 @@
+﻿using AOC.Helpers.Contracts;
+using AOC.Helpers.Implementations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Day2
+{
+    public class GameTaskRunner : ITaskRunner
+    {
+        private readonly int _red = 12;
+        private readonly int _green = 13;
+        private readonly int _blue = 14;
+
+        public IFileReader FileReader { get; set; }
+
+        public GameTaskRunner(IFileReader fileReader)
+        {
+            FileReader = fileReader;
+        }
+
+        public void Run()
+        {
+            var games = FileReader.ReadDocumentAsList();
+
+            int sumOfViableGameIds = 0;
+            bool isViable = false;
+
+            foreach (var game in games) //all the games
+            {
+                var gameInfo = game.Split(':'); //split string to get Id of the game
+
+                var gameObjectId = Int32.Parse(gameInfo[0].Replace("Game ", ""));
+
+                var gameResults = gameInfo[1].Split(";");
+
+                foreach (var gameResult in gameResults) //all the rounds in a game
+                {
+                    var round = gameResult.Split(",");
+
+                    foreach (var roundResult in round) //all results in a round
+                    {
+                        if (roundResult.Contains("red"))
+                        {
+                            var numberOfReds = Int32.Parse(roundResult.Replace(" red", ""));
+                            if (numberOfReds > _red)
+                            {
+                                isViable = false;
+                                break;
+                            }
+                            else
+                                isViable = true;
+                        }
+                        else if (roundResult.Contains("blue"))
+                        {
+                            var numberOfBlues = Int32.Parse(roundResult.Replace(" blue", ""));
+                            if (numberOfBlues > _blue)
+                            {
+                                isViable = false;
+                                break;
+                            }
+                            else
+                                isViable = true;
+                        }
+                        else if (roundResult.Contains("green"))
+                        {
+                            var numberOfGreens = Int32.Parse(roundResult.Replace(" green", ""));
+                            if (numberOfGreens > _green)
+                            {
+                                isViable = false;
+                                break;
+                            }
+                            else
+                                isViable = true;
+                        }
+                    }
+                    if (!isViable)
+                        break;
+                }
+
+                Console.WriteLine($"{gameObjectId}: {isViable.ToString()}");
+
+                if (isViable)
+                {
+                    sumOfViableGameIds += gameObjectId;
+                }
+            }
+
+            Console.WriteLine(sumOfViableGameIds.ToString());
+        }
+
+        //Example: "Game 2: 6 red, 6 blue, 2 green; 1 blue, 1 red; 6 green, 1 red, 10 blue"
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -3,6 +3,7 @@ using AdventOfCode.Day2;
 using AOC.Helpers.Contracts;
 using AOC.Helpers.Implementations;
 using Ninject;
+using System.Diagnostics;
 using System.Reflection;
 using System.Text.Json;
 using System.Xml;
@@ -12,16 +13,23 @@ namespace AdventOfCode
 {
     public class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Console.WriteLine("Creating kernel...");
             IKernel kernel = new StandardKernel(new DIModule());
             try
             {
-                ITaskRunner runner = new TrebuchetTask2Runner(new FileReader());
-                runner.Run();
+                //var sw = new Stopwatch();
+                //sw.Start();
+                //ITaskRunner runner = new TrebuchetTask2Runner(new FileReader("C:\\Users\\mpara\\Desktop\\adventOfCode\\calibrationDocuemnt.txt"));
+                //runner.Run();
+                //sw.Stop();
+                //Console.WriteLine(sw.Elapsed.ToString());
+
+                ITaskRunner gameRunner = new GameTaskRunner(new FileReader("C:\\Users\\mpara\\Desktop\\adventOfCode\\gameDocument.txt"));
+                gameRunner.Run();
             }
-            catch (Exception ex) 
+            catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
                 Console.WriteLine(ex.StackTrace);
